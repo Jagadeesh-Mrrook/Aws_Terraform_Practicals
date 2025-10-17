7adda7ecb69fc36efc88c5276952b944bfad313b
Jagadeesh T P <jagadeeshgowda002@gmail.com>
2025-10-17 23:00:38 +0530

    removed  arouond the values of cidr and az while calling the variables

--- Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
+++ Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
@@ -29,8 +29,8 @@ resource "aws_vpc" "first_vpc" {
 
 resource "aws_subnet" "tf_public_subnet1" {
   vpc_id     = aws_vpc.first_vpc.id
-  cidr_block = "var.public_subnet_cidr1"
-  availability_zone = "var.availability_zone1"
+  cidr_block = var.public_subnet_cidr1
+  availability_zone = var.availability_zone1
   map_public_ip_on_launch = true
 
 
@@ -41,8 +41,8 @@ resource "aws_subnet" "tf_public_subnet1" {
 
 resource "aws_subnet" "tf_public_subnet2" {
   vpc_id     = aws_vpc.first_vpc.id
-  cidr_block = "var.public_subnet_cidr2"
-  availability_zone = "var.availability_zone2"
+  cidr_block = var.public_subnet_cidr2
+  availability_zone = var.availability_zone2
   map_public_ip_on_launch = true
 
   tags = {
@@ -53,8 +53,8 @@ resource "aws_subnet" "tf_public_subnet2" {
 
 resource "aws_subnet" "tf_private_subnet1" {
   vpc_id     = aws_vpc.first_vpc.id
-  cidr_block = "var.private_subnet_cidr1"
-  availability_zone = "var.availability_zone1"
+  cidr_block = var.private_subnet_cidr1
+  availability_zone = var.availability_zone1
   map_public_ip_on_launch = false
 
   tags = {
@@ -65,8 +65,8 @@ resource "aws_subnet" "tf_private_subnet1" {
 
 resource "aws_subnet" "tf_private_subnet2" {
   vpc_id     = aws_vpc.first_vpc.id
-  cidr_block = "var.private_subnet_cidr2"
-  availability_zone = "var.availability_zone2"
+  cidr_block = var.private_subnet_cidr2
+  availability_zone = var.availability_zone2
   map_public_ip_on_launch = true
 
   tags = {
