111e2e37065f287fea3b4125fb21e6cd8ce33145
Jagadeesh T P <jagadeeshgowda002@gmail.com>
2025-10-17 22:53:45 +0530

    on main.tf in subnet resource, vpc_id correctly mentioned

--- Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
+++ Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
@@ -28,7 +28,7 @@ resource "aws_vpc" "first_vpc" {
 }
 
 resource "aws_subnet" "tf_public_subnet1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.first_vpc.id
   cidr_block = "var.public_subnet_cidr1"
   availability_zone = "var.availability_zone1"
   map_public_ip_on_launch = true
@@ -40,7 +40,7 @@ resource "aws_subnet" "tf_public_subnet1" {
 }
 
 resource "aws_subnet" "tf_public_subnet2" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.first_vpc.id
   cidr_block = "var.public_subnet_cidr2"
   availability_zone = "var.availability_zone2"
   map_public_ip_on_launch = true
@@ -52,7 +52,7 @@ resource "aws_subnet" "tf_public_subnet2" {
 }
 
 resource "aws_subnet" "tf_private_subnet1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.first_vpc.id
   cidr_block = "var.private_subnet_cidr1"
   availability_zone = "var.availability_zone1"
   map_public_ip_on_launch = false
@@ -64,7 +64,7 @@ resource "aws_subnet" "tf_private_subnet1" {
 }
 
 resource "aws_subnet" "tf_private_subnet2" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.first_vpc.id
   cidr_block = "var.private_subnet_cidr2"
   availability_zone = "var.availability_zone2"
   map_public_ip_on_launch = true
