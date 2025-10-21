a7ed4d6eb5a7717f6dc46904af96bb07821ccff5
Jagadeesh T P <jagadeeshgowda002@gmail.com>
2025-10-21 21:31:47 +0530

    security group added to terraform

--- Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
+++ Terraform_Practicals.md/Terraform_Practicals_code/terraform-vpc-demo/main.tf
@@ -113,17 +113,17 @@ resource "aws_route_table" "terraform-private-rt" {
   }
 }
 
-resource "aws_route" "Public-Internet1" {
-  route_table_id = aws_route_table.terraform-public-rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.terraform-nat-gateway1.id
-}
+# resource "aws_route" "Public-Internet1" {
+#   route_table_id = aws_route_table.terraform-public-rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.terraform-nat-gateway1.id
+# }
 
-resource "aws_route" "Public-Internet2" {
-  route_table_id = aws_route_table.terraform-public-rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.terraform-nat-gateway2.id
-}
+# resource "aws_route" "Public-Internet2" {
+#   route_table_id = aws_route_table.terraform-public-rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.terraform-nat-gateway2.id
+# }
 resource "aws_route_table_association" "rta-private-subnet1" {
   subnet_id      = aws_subnet.tf_private_subnet1.id
   route_table_id = aws_route_table.terraform-private-rt.id
@@ -134,50 +134,104 @@ resource "aws_route_table_association" "rta-private-subnet2" {
   route_table_id = aws_route_table.terraform-private-rt.id
 }
 
-##Elastic IP-1 for NAT Gateway
-resource "aws_eip" "eip_nat_gateway" {
-  domain = "vpc"
+# ##Elastic IP-1 for NAT Gateway
+# resource "aws_eip" "eip_nat_gateway" {
+#   domain = "vpc"
 
+#   tags = {
+#     Name = "terraform-eip-nat-gateway1"
+#   }
+# }
+
+# #NAT Gateway-1 Creation
+
+# resource "aws_nat_gateway" "terraform-nat-gateway1" {
+#   allocation_id = aws_eip.eip_nat_gateway.id
+#   subnet_id     = aws_subnet.tf_public_subnet1.id
+
+#   tags = {
+#     Name = "Terraform-nat-gateway-Subnet1"
+#   }
+
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.terraform-igw]
+# }
+
+# ##Elastic IP-1 for NAT Gateway
+# resource "aws_eip" "eip_nat_gateway2" {
+#   domain = "vpc"
+
+#   tags = {
+#     Name = "terraform-eip-nat-gateway2"
+#   }
+# }
+
+# #NAT Gateway-2 Creation
+
+# resource "aws_nat_gateway" "terraform-nat-gateway2" {
+#   allocation_id = aws_eip.eip_nat_gateway2.id
+#   subnet_id     = aws_subnet.tf_public_subnet2.id
+
+#   tags = {
+#     Name = "Terraform-nat-gateway-Subnet2"
+#   }
+
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.terraform-igw]
+# }
+
+#Security Group Creation
+resource "aws_security_group" "terraform_sg" {
+  name        = "Terraform-SG"
+  vpc_id      = aws_vpc.first_vpc.id
   tags = {
-    Name = "terraform-eip-nat-gateway1"
+    Name = "Terraform-SG"
   }
 }
 
-#NAT Gateway-1 Creation
-
-resource "aws_nat_gateway" "terraform-nat-gateway1" {
-  allocation_id = aws_eip.eip_nat_gateway.id
-  subnet_id     = aws_subnet.tf_public_subnet1.id
-
-  tags = {
-    Name = "Terraform-nat-gateway-Subnet1"
-  }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.terraform-igw]
+#Ingress Rule to allow SSH from within VPC
+resource "aws_security_group_rule" "ssh_rule" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.terraform_sg.id
+  description       = "Allow SSH from within VPC"  
 }
 
-##Elastic IP-1 for NAT Gateway
-resource "aws_eip" "eip_nat_gateway2" {
-  domain = "vpc"
-
-  tags = {
-    Name = "terraform-eip-nat-gateway2"
-  }
+#Ingress Rule to allow http from anywhere
+resource "aws_security_group_rule" "http_rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.terraform_sg.id
+  description       = "Allow SSH from within VPC"  
 }
 
-#NAT Gateway-2 Creation
-
-resource "aws_nat_gateway" "terraform-nat-gateway2" {
-  allocation_id = aws_eip.eip_nat_gateway2.id
-  subnet_id     = aws_subnet.tf_public_subnet2.id
-
-  tags = {
-    Name = "Terraform-nat-gateway-Subnet2"
-  }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.terraform-igw]
+#Ingress Rule to allow https from anywhere
+resource "aws_security_group_rule" "https_rule" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.terraform_sg.id
+  description       = "Allow SSH from within VPC"  
 }
+
+
+resource "aws_security_group_rule" "egress_rule" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.terraform_sg.id
+  description       = "Allow all outbound traffic"
+}
+
